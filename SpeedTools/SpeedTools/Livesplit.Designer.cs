8bfdfe6d9877f68f19093fb24f3918ad53b088e2
FromDarkHell <treepuncher0705@gmail.com>
2016-12-13 19:42:44 -0600

    Add Livesplit Support

--- SpeedTools/SpeedTools/Livesplit.Designer.cs
+++ SpeedTools/SpeedTools/Livesplit.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.Back = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
@@ -37,6 +38,11 @@
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.portToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
@@ -47,6 +53,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Start";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Back
             // 
@@ -56,6 +63,7 @@
             this.Back.TabIndex = 1;
             this.Back.Text = "Back";
             this.Back.UseVisualStyleBackColor = true;
+            this.Back.Click += new System.EventHandler(this.Back_Click);
             // 
             // richTextBox1
             // 
@@ -73,6 +81,7 @@
             this.button3.TabIndex = 3;
             this.button3.Text = "Split";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button2
             // 
@@ -82,6 +91,7 @@
             this.button2.TabIndex = 4;
             this.button2.Text = "Unsplit";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button4
             // 
@@ -91,6 +101,7 @@
             this.button4.TabIndex = 5;
             this.button4.Text = "Skip Split";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
@@ -100,6 +111,7 @@
             this.button5.TabIndex = 6;
             this.button5.Text = "Pause";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
@@ -109,6 +121,7 @@
             this.button6.TabIndex = 7;
             this.button6.Text = "Reset";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
@@ -118,12 +131,45 @@
             this.button7.TabIndex = 8;
             this.button7.Text = "Resume";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(1, 1);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(70, 38);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "Options";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.portToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(100, 26);
+            // 
+            // portToolStripMenuItem
+            // 
+            this.portToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripTextBox1});
+            this.portToolStripMenuItem.Name = "portToolStripMenuItem";
+            this.portToolStripMenuItem.Size = new System.Drawing.Size(99, 22);
+            this.portToolStripMenuItem.Text = "Port:";
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(100, 23);
+            this.toolStripTextBox1.Text = "16834";
             // 
             // Livesplit
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(292, 222);
+            this.Controls.Add(this.button8);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
@@ -135,6 +181,8 @@
             this.Controls.Add(this.button1);
             this.Name = "Livesplit";
             this.Text = "Livesplit";
+            this.Load += new System.EventHandler(this.Livesplit_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -150,5 +198,9 @@
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem portToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
     }
 }
--- SpeedTools/SpeedTools/Livesplit.cs
+++ SpeedTools/SpeedTools/Livesplit.cs
@@ -7,10 +7,108 @@ namespace SpeedTools
 {
     public partial class Livesplit : Form
     {
-        
+        #region Variables
+        ProcessStartInfo pythonInfo = new ProcessStartInfo();
+        Process python;
+        string port = "16834";
+        string pypath = "C:\\Python27\\python.exe";
+        string portpath = "C:\\SpeedTools\\SpeedTools\\SpeedTools\\SpeedTools\\SpeedTools-Python\\livesplitport.txt";
+        #endregion
         public Livesplit()
         {
             InitializeComponent();
+        }
+        private void Back_Click(object sender, EventArgs e) {
+            this.Hide();
+            var Inf = new Main();
+            Inf.Closed += (s, args) => this.Close();
+            Inf.Show();
+        }
+        private void button1_Click(object sender, EventArgs e) {
+            #region File Management
+            System.Threading.Thread.Sleep(10);
+            if (!File.Exists(portpath))
+            {
+                string port = toolStripTextBox1.Text;
+                File.WriteAllText(portpath, port);
+            }
+            #endregion
+            // Start Timer
+            pythonInfo.Arguments = @"C:\\SpeedTools\\SpeedTools\\SpeedTools\\SpeedTools\\SpeedTools-Python\\start.py";
+            pythonInfo.CreateNoWindow = false;
+            pythonInfo.UseShellExecute = true;
+            richTextBox1.Text = "Timer Starting\r\n";
+            python = Process.Start(pythonInfo);
+            python.WaitForExit();
+            python.Close();
+        }
+        private void button3_Click(object sender, EventArgs e) {
+            // Split
+            pythonInfo.Arguments = @"C:\\SpeedTools\\SpeedTools\\SpeedTools\\SpeedTools\\SpeedTools-Python\\split.py";
+            pythonInfo.CreateNoWindow = false;
+            pythonInfo.UseShellExecute = true;
+            richTextBox1.Text = "Timer Starting\r\n";
+            python = Process.Start(pythonInfo);
+            python.WaitForExit();
+            python.Close();
+        }
+        private void button2_Click(object sender, EventArgs e) {
+            // Unsplit
+            pythonInfo.Arguments = @"C:\\SpeedTools\\SpeedTools\\SpeedTools\\SpeedTools\\SpeedTools-Python\\unsplit.py";
+            pythonInfo.CreateNoWindow = false;
+            pythonInfo.UseShellExecute = true;
+            richTextBox1.Text = "Timer Starting\r\n";
+            python = Process.Start(pythonInfo);
+            python.WaitForExit();
+            python.Close();
+        }
+        private void button4_Click(object sender, EventArgs e) {
+            // Skip Split
+            pythonInfo.Arguments = @"C:\\SpeedTools\\SpeedTools\\SpeedTools\\SpeedTools\\SpeedTools-Python\\skipsplit.py";
+            pythonInfo.CreateNoWindow = false;
+            pythonInfo.UseShellExecute = true;
+            richTextBox1.Text = "Timer Starting\r\n";
+            python = Process.Start(pythonInfo);
+            python.WaitForExit();
+            python.Close();
+        }
+        private void button5_Click(object sender, EventArgs e) {
+            // Pause
+            pythonInfo.Arguments = @"C:\\SpeedTools\\SpeedTools\\SpeedTools\\SpeedTools\\SpeedTools-Python\\pause.py";
+            pythonInfo.CreateNoWindow = false;
+            pythonInfo.UseShellExecute = true;
+            richTextBox1.Text = "Timer Starting\r\n";
+            python = Process.Start(pythonInfo);
+            python.WaitForExit();
+            python.Close();
+        }
+        private void button7_Click(object sender, EventArgs e) {
+            // Resume
+            pythonInfo.Arguments = @"C:\\SpeedTools\\SpeedTools\\SpeedTools\\SpeedTools\\SpeedTools-Python\\skipsplit.py";
+            pythonInfo.CreateNoWindow = false;
+            pythonInfo.UseShellExecute = true;
+            richTextBox1.Text = "Timer Starting\r\n";
+            python = Process.Start(pythonInfo);
+            python.WaitForExit();
+            python.Close();
+        }
+        private void button6_Click(object sender, EventArgs e) {
+            // Reset
+            pythonInfo.Arguments = @"C:\\SpeedTools\\SpeedTools\\SpeedTools\\SpeedTools\\SpeedTools-Python\\reset.py";
+            pythonInfo.CreateNoWindow = false;
+            pythonInfo.UseShellExecute = true;
+            richTextBox1.Text = "Timer Starting\r\n";
+            python = Process.Start(pythonInfo);
+            python.WaitForExit();
+            python.Close();
+        }
+        private void button8_Click(object sender, EventArgs e) {
+            // Port Menu
+            contextMenuStrip1.Show(Cursor.Position.X, Cursor.Position.Y);
+        }
+        private void Livesplit_Load(object sender, EventArgs e)
+        {
+            pythonInfo.FileName = @"C:\Python27\python.exe";
         }
     }
 }
