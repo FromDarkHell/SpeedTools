3ec7f1eda265a74a053eae6e99511d3770eaf5c5
FromDarkHell <treepuncher0705@gmail.com>
2016-12-14 21:26:22 -0600

    Add SRL Bingo Support

--- SpeedTools/SpeedTools/SRL.Designer.cs
+++ SpeedTools/SpeedTools/SRL.Designer.cs
@@ -28,19 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Browser = new System.Windows.Forms.WebBrowser();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(85, 33);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(394, 27);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(241, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Game Name:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(2, -4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(233, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Get Board";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Browser
+            // 
+            this.Browser.Location = new System.Drawing.Point(2, 66);
+            this.Browser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.Browser.Name = "Browser";
+            this.Browser.Size = new System.Drawing.Size(620, 504);
+            this.Browser.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(374, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(233, 34);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Get URL (Copy)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // SRL
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(394, 303);
+            this.ClientSize = new System.Drawing.Size(619, 569);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Browser);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
             this.Name = "SRL";
             this.Text = "SpeedTools - SRL";
+            this.Load += new System.EventHandler(this.SRL_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.WebBrowser Browser;
+        private System.Windows.Forms.Button button2;
     }
 }
--- SpeedTools/SpeedTools/SRL.cs
+++ SpeedTools/SpeedTools/SRL.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
+﻿using System.Windows.Forms;
+using System;
 namespace SpeedTools
 {
     public partial class SRL : Form
@@ -16,5 +8,228 @@ namespace SpeedTools
         {
             InitializeComponent();
         }
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            string gaem = richTextBox1.Text;
+            if (gaem == null)
+            {
+                Browser.Url = new System.Uri("http://speedruntools.com/bingo/generic", System.UriKind.Absolute);
+            }
+            if (gaem != null)
+            {
+                if (gaem.Contains("Link to the Past"))
+                {
+                    Browser.Url = new System.Uri("http://speedruntools.com/bingo/alttp", System.UriKind.Absolute);
+                }
+                else
+                {
+                    if (gaem.Contains("Antichamber"))
+                    {
+                        Browser.Url = new System.Uri("http://speedruntools.com/bingo/antichamber", System.UriKind.Absolute);
+                    }
+                    else
+                    {
+                        if (gaem.Contains("Banjo-Tooie"))
+                        {
+                            Browser.Url = new System.Uri("http://speedruntools.com/bingo/bt", System.UriKind.Absolute);
+                        }
+                        else
+                        {
+                            if (gaem.Contains("Bully: SE"))
+                            {
+                                Browser.Url = new System.Uri("http://speedruntools.com/bingo/bully", System.UriKind.Absolute);
+                            }
+                            else
+                            {
+                                if (gaem.Contains("Bully: SE Short"))
+                                {
+                                    Browser.Url = new System.Uri("http://speedruntools.com/bingo/bully-short", System.UriKind.Absolute);
+                                }
+                                else
+                                {
+                                    if (gaem.Contains("ET"))
+                                    {
+                                        Browser.Url = new System.Uri("http://speedruntools.com/bingo/et", System.UriKind.Absolute);
+                                    }
+                                    else
+                                    {
+                                        if (gaem.Contains("Friday 13th"))
+                                        {
+                                            Browser.Url = new System.Uri("http://speedruntools.com/bingo/ft13", System.UriKind.Absolute);
+                                        }
+                                        else
+                                        {
+                                            if (gaem.Contains("HP2"))
+                                            {
+                                                Browser.Url = new System.Uri("http://speedruntools.com/bingo/hp2", System.UriKind.Absolute);
+                                            }
+                                            else
+                                            {
+                                                if (gaem.Contains("LEGO Star Wars"))
+                                                {
+                                                    Browser.Url = new System.Uri("http://speedruntools.com/bingo/lego-starwars", System.UriKind.Absolute);
+                                                }
+                                                else
+                                                {
+                                                    if (gaem.Contains("Superstar Saga"))
+                                                    {
+                                                        Browser.Url = new System.Uri("http://speedruntools.com/bingo/mlss", System.UriKind.Absolute);
+                                                    }
+                                                    else
+                                                    {
+                                                        if (gaem.Contains("Majora's Mask"))
+                                                        {
+                                                            Browser.Url = new System.Uri("http://speedruntools.com/bingo/mm", System.UriKind.Absolute);
+                                                        }
+                                                        else
+                                                        {
+                                                            if (gaem.Contains("Metroid Prime"))
+                                                            {
+                                                                Browser.Url = new System.Uri("http://speedruntools.com/bingo/mp1", System.UriKind.Absolute);
+                                                            }
+                                                            else
+                                                            {
+                                                                if (gaem.Contains("Oblivion"))
+                                                                {
+                                                                    Browser.Url = new System.Uri("http://speedruntools.com/bingo/oblivion", System.UriKind.Absolute);
+                                                                }
+                                                                else
+                                                                {
+                                                                    if (gaem.Contains("Pac-World 2"))
+                                                                    {
+                                                                        Browser.Url = new System.Uri("http://speedruntools.com/bingo/pacworld2", System.UriKind.Absolute);
+                                                                    }
+                                                                    else
+                                                                    {
+                                                                        if (gaem.Contains("Paper Mario"))
+                                                                        {
+                                                                            Browser.Url = new System.Uri("http://speedruntools.com/bingo/pmario", System.UriKind.Absolute);
+                                                                        }
+                                                                        else
+                                                                        {
+                                                                            if (gaem.Contains("TTYD"))
+                                                                            {
+                                                                                Browser.Url = new System.Uri("http://speedruntools.com/bingo/ttyd", System.UriKind.Absolute);
+                                                                            }
+                                                                            else
+                                                                            {
+                                                                                if (gaem.Contains("Red/Blue"))
+                                                                                {
+                                                                                    Browser.Url = new System.Uri("http://speedruntools.com/bingo/pkmn-redblue", System.UriKind.Absolute);
+                                                                                }
+                                                                                else
+                                                                                {
+                                                                                    if (gaem.Contains("Ruby/Sapphire"))
+                                                                                    {
+                                                                                        Browser.Url = new System.Uri("http://speedruntools.com/bingo/pkmn-rubysapphire", System.UriKind.Absolute);
+                                                                                    }
+                                                                                    else
+                                                                                    {
+                                                                                        if (gaem.Contains("Snap"))
+                                                                                        {
+                                                                                            Browser.Url = new System.Uri("http://speedruntools.com/bingo/pkmn-snap", System.UriKind.Absolute);
+                                                                                        }
+                                                                                        else
+                                                                                        {
+                                                                                            if (gaem.Contains("Rayman 3"))
+                                                                                            {
+                                                                                                Browser.Url = new System.Uri("http://speedruntools.com/bingo/rayman3", System.UriKind.Absolute);
+                                                                                            }
+                                                                                            else
+                                                                                            {
+                                                                                                if (gaem.Contains("SHAR"))
+                                                                                                {
+                                                                                                    Browser.Url = new System.Uri("http://speedruntools.com/bingo/simpsons-hr", System.UriKind.Absolute);
+                                                                                                }
+                                                                                                else
+                                                                                                {
+                                                                                                    if (gaem.Contains("Sonic R"))
+                                                                                                    {
+                                                                                                        Browser.Url = new System.Uri("http://speedruntools.com/bingo/sonicr", System.UriKind.Absolute);
+                                                                                                    }
+                                                                                                    else
+                                                                                                    {
+                                                                                                        if (gaem.Contains("SA2B"))
+                                                                                                        {
+                                                                                                            Browser.Url = new System.Uri("http://speedruntools.com/bingo/sab2", System.UriKind.Absolute);
+                                                                                                        }
+                                                                                                        else
+                                                                                                        {
+                                                                                                            if (gaem.Contains("SM64"))
+                                                                                                            {
+                                                                                                                Browser.Url = new System.Uri("http://speedruntools.com/bingo/sm64", System.UriKind.Absolute);
+                                                                                                            }
+                                                                                                            else
+                                                                                                            {
+                                                                                                                if (gaem.Contains("Symphonia"))
+                                                                                                                {
+                                                                                                                    Browser.Url = new System.Uri("http://speedruntools.com/bingo/tos", System.UriKind.Absolute);
+                                                                                                                }
+                                                                                                                else
+                                                                                                                {
+                                                                                                                    if (gaem.Contains("Talos"))
+                                                                                                                    {
+                                                                                                                        Browser.Url = new System.Uri("http://speedruntools.com/bingo/talos", System.UriKind.Absolute);
+                                                                                                                    }
+                                                                                                                    else
+                                                                                                                    {
+                                                                                                                        if (gaem.Contains("Ty"))
+                                                                                                                        {
+                                                                                                                            Browser.Url = new System.Uri("http://speedruntools.com/bingo/ty", System.UriKind.Absolute);
+                                                                                                                        }
+                                                                                                                        else
+                                                                                                                        {
+                                                                                                                            if (gaem.Contains("Warframe"))
+                                                                                                                            {
+                                                                                                                                Browser.Url = new System.Uri("http://speedruntools.com/bingo/warframe", System.UriKind.Absolute);
+                                                                                                                            }
+                                                                                                                            else
+                                                                                                                            {
+                                                                                                                                Browser.Url = new System.Uri("http://speedruntools.com/bingo/wl4", System.UriKind.Absolute);
+                                                                                                                            }
+                                                                                                                        }
+                                                                                                                    }
+                                                                                                                }
+                                                                                                            }
+                                                                                                        }
+                                                                                                    }
+                                                                                                }
+                                                                                            }
+                                                                                        }
+                                                                                    }
+                                                                                }
+                                                                            }
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                button2.Visible = true;
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+                string url = Browser.Url.ToString();
+                if (url != null)
+                {
+                    MessageBox.Show(url + " is the URL");
+                    Clipboard.SetDataObject(url, true);
+                }
+            }
+
+        private void SRL_Load(object sender, EventArgs e)
+        {
+            button2.Visible = false;
+        }
     }
-}
+    }
